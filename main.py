1afb692c8a64b37b2ab0eb5410f0f9073b71b3e8
eunspring <tkell3210@gmail.com>
2023-11-08 16:33:51 +0900

    lotto commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import random
+import tkinter
+import tkinter.font
+
+lotto_num = range(1, 46)
+
+def buttonClick():
+    print(random.sample(lotto_num, 6))
+
+window = tkinter.Tk()
+window.title("lotto")
+window.geometry("400x200")
+window.resizable(False, False)
+
+button = tkinter.Button(window, overrelief="solid", text = "번호확인", width=15, command=buttonClick,
+                        repeatdelay=1000, repeatinterval=100)
+button.pack()
+
+window.mainloop()
